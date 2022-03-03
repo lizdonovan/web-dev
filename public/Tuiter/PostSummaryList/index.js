924553f7260efeeba2a2bf89e8417885de2b8652
donovanel <donovan.el@northeastern.edu>
2022-03-02 23:47:04 -0500

    post summary wip

--- public/Tuiter/PostSummaryList/index.js
+++ public/Tuiter/PostSummaryList/index.js
@@ -1,5 +1,13 @@
-const PostSummaryList = (posts) => {
+import post from "./post.js";
 
+const PostSummaryList = post => {
 
+    return(`
+        <ul>
+           
+
+        </ul>
+    `);
 
 }
+export default PostSummaryList;
