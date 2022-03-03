0a45cdf3e9f6bcf2e9963679b6261cef96292402
donovanel <donovan.el@northeastern.edu>
2022-03-02 23:04:26 -0500

    stash

--- public/Tuiter/PostSummaryList/PostSummaryItem.js
+++ public/Tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,7 +1,19 @@
 const PostSummaryItem = post => {
     return(`
         <li>
-        
+            <div class="card bg-light border-white">
+                    <div class="card-body">
+
+                        <img src="images/jquery.png" alt="react js logo" width="100" class="float-end">
+
+                        <p class="text-white">${post.topic}</p>
+                        <p class="fw-bold text-white">${post.userName} - ${post.time}</p>
+                        <p class="fw-bold text-white">
+                            ${post.title}
+                        </p>
+
+                    </div>
+                </div>
         </li>
     `);
 
