651aa24ba27f055b49ffa216c0102fa54ac48e2b
donovanel <donovan.el@northeastern.edu>
2022-03-02 22:01:01 -0500

    post summary list item wip

--- public/Tuiter/PostSummaryList/PostSummaryItem.js
+++ public/Tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,5 +1,9 @@
-const PostSummaryItem = (post) => {
+const PostSummaryItem = post => {
+    return(`
+        <li>
+        
+        </li>
+    `);
 
-    let htmlPost = "{" + post.topic + post.userName + post.time + post.image + post.title + "}";
-    return htmlPost;
 }
+export default PostSummaryItem;
