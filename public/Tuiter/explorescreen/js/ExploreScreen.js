fbeb6f667279a4bc429f3e3a28c125b0d5053233
donovanel <donovan.el@northeastern.edu>
2022-03-04 20:09:40 -0500

    active working now

--- public/Tuiter/explorescreen/js/ExploreScreen.js
+++ public/Tuiter/explorescreen/js/ExploreScreen.js
@@ -6,7 +6,7 @@ import WhoToFollowList from "../../whotofollowlist/index.js";
     $('#wd-explore').append(`
         <div class="row mt-2">
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-            ${NavigationSidebar()}
+            ${NavigationSidebar("explore")}
         </div>
         <div class="col-10 col-md-6">
             ${ExploreComponent()}
--- public/Tuiter/navigationsidebar/index.js
+++ public/Tuiter/navigationsidebar/index.js
@@ -1,37 +1,35 @@
 const NavigationSidebar = (active) => {
 
-    const addActive = $(active);
-    addActive.addClass("active");
 
     return(`
             <div class="list-group">
                 <!-- Twitter bird -->
-                <a class="list-group-item bg-light fg-color-white" href="/">
+                <a class="list-group-item fg-color-white ${active === 'tuiter' ? 'active' : ''}" href="/">
                     <i class="fab fa-twitter"></i></a>
             <!-- continue the rest of the list -->
                 <!-- Home -->
-                <a id="home" class="list-group-item bg-light fg-color-white" href="../homescreen/home.html">
+                <a id="home" class="list-group-item fg-color-white ${active === 'home' ? 'active' : ''}" href="../homescreen/home.html">
                     <i class="fa-solid fa-house"></i>Home</a>
                 <!-- Explore -->
-                <a id="explore" class="list-group-item fg-color-white" href="../explorescreen/explore.html">
+                <a id="explore" class="list-group-item fg-color-white ${active === 'explore' ? 'active' : ''}" href="../explorescreen/explore.html">
                     <i class="fa-solid fa-hashtag"></i>Explore</a>
                 <!-- Notifications -->
-                <a class="list-group-item bg-light fg-color-white" href="">
+                <a class="list-group-item fg-color-white ${active === 'notifications' ? 'active' : ''}" href="">
                     <i class="fa-solid fa-bell"></i>Notifications</a>
                 <!-- Messages -->
-                <a class="list-group-item bg-light fg-color-white" href="">
+                <a class="list-group-item fg-color-white ${active === 'messages' ? 'active' : ''}" href="">
                     <i class="fa-solid fa-envelope"></i>Messages</a>
                 <!-- Bookmarks -->
-                <a class="list-group-item bg-light fg-color-white" href="">
+                <a class="list-group-item fg-color-white ${active === 'bookmarks' ? 'active' : ''}" href="">
                     <i class="fa-solid fa-bookmark"></i>Bookmarks</a>
                 <!-- Lists -->
-                <a class="list-group-item bg-light fg-color-white" href="">
+                <a class="list-group-item fg-color-white ${active === 'lists' ? 'active' : ''}" href="">
                     <i class="fa-solid fa-list"></i>Lists</a>
                 <!-- Profile -->
-                <a class="list-group-item bg-light fg-color-white" href="">
+                <a class="list-group-item fg-color-white ${active === 'profile' ? 'active' : ''}" href="">
                     <i class="fa-solid fa-user"></i>Profile</a>
                 <!-- More -->
-                <a class="list-group-item bg-light fg-color-white" href="">
+                <a class="list-group-item fg-color-white ${active === 'more' ? 'active' : ''}" href="">
                     <i class="fa-solid fa-ellipsis"></i>More</a>   
             </div>
             <div class="d-grid mt-2">
