646604ea1dcaa5da39dc82899ec1990bb48e14b6
donovanel <donovan.el@northeastern.edu>
2022-03-15 17:06:22 -0400

    hello world js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 
+import HelloWorld from "./components/hello-world";
 
 import './App.css';
 
 function App() {
   return (
       <div className="container">
-        <h1>Hello World!</h1>
+          <HelloWorld/>
       </div>
 
   );
