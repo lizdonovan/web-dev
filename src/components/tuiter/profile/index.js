5f7533d74d0d69c8d8c47775566623c0d9a4467d
donovanel <donovan.el@northeastern.edu>
2022-03-30 08:59:46 -0400

    edit profile components

--- src/components/tuiter/profile/index.js
+++ src/components/tuiter/profile/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-
-
-
-
+import {useSelector} from "react-redux";
 
 const Profile = () => {
+    const profile = useSelector(state => state.profile);
     return(
         <>
-            <div>
+            <div className="card">
+                <div className="card-body">
+                    <h3 className="card-title">profile.firstName</h3>
+                    <h4 className="card-subtitle">23423 Tuits</h4>
 
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+
+                </div>
             </div>
         </>
     );
