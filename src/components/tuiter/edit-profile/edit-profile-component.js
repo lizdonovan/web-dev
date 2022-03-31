a764c5fd5bf35a470b52ef12f86aad0ab321cd79
donovanel <donovan.el@northeastern.edu>
2022-03-31 09:13:59 -0400

    on change event but not working

--- src/components/tuiter/edit-profile/edit-profile-component.js
+++ src/components/tuiter/edit-profile/edit-profile-component.js
@@ -1,7 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {useDispatch} from "react-redux";
 
 const EditProfileComponent = ({profile}) => {
+    const dispatch = useDispatch();
+
+    const saveProfile = () => {
+        dispatch({type: 'save-profile', profile});
+    };
     return(
         <>
             <div className="card">
@@ -15,7 +21,7 @@ const EditProfileComponent = ({profile}) => {
 
                         <h5 className="col">Edit Profile</h5>
 
-                        <button className="col-2 float-end btn btn-outline-dark" >Save</button>
+                        <button className="col-2 float-end btn btn-outline-dark" onClick={saveProfile}>Save</button>
                     </div>
 
                     <img className="card-img-top" src={profile.bannerPicture}/>
@@ -28,29 +34,32 @@ const EditProfileComponent = ({profile}) => {
 
                     <div className="row mb-4">
                         <label htmlFor="first">First Name</label>
-                        <input type="text" id="name" placeholder={profile.firstName}/>
+                        <input type="text" id="name" placeholder={profile.firstName} onChange={(event => ({...profile, firstName: event.target.value}))}/>
                     </div>
 
                     <div className="row mb-4">
                         <label htmlFor="last">Last Name</label>
-                        <input type="text" id="last" placeholder={profile.lastName}/>
+                        <input type="text" id="last" placeholder={profile.lastName} onChange={(event => ({...profile, lastName: event.target.value}))}/>
                     </div>
 
                     <div className="row mb-4">
                         <label htmlFor="bio">Bio</label>
                         <textarea id="bio"
-                        placeholder={profile.bio}>
+                        placeholder={profile.bio}
+                                  onChange={(event => ({...profile, bio: event.target.value}))}>
                         </textarea>
                     </div>
 
                     <div className="row mb-4">
                         <label htmlFor="location">Location</label>
-                        <input type="text" id="location" placeholder={profile.location}/>
+                        <input type="text" id="location" placeholder={profile.location}
+                               onChange={(event => ({...profile, location: event.target.value}))}/>
                     </div>
 
                     <div className="row mb-4">
-                        <label htmlFor="dob">Last Name</label>
-                        <input type="text" id="dob" placeholder={profile.dateOfBirth}/>
+                        <label htmlFor="dob">Date of Birth</label>
+                        <input type="text" id="dob" placeholder={profile.dateOfBirth}
+                               onChange={(event => ({...profile, dateOfBirth: event.target.value}))}/>
                     </div>
 
                 </div>
--- src/components/tuiter/reducers/profile-reducer.js
+++ src/components/tuiter/reducers/profile-reducer.js
@@ -1,7 +1,12 @@
 import profile from '../data/profile.json';
 
-const profileReducer = (state = profile) => {
-    return(state);
+const profileReducer = (state = profile, action) => {
+    switch (action.type) {
+        case 'save-profile':
+            return[action.profile];
+        default:
+            return profile;
+    }
 };
 
 export default profileReducer;
