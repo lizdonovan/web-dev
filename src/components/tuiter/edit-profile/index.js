fe27f31727ea1357425779c444cdd9b450a1f0cf
donovanel <donovan.el@northeastern.edu>
2022-03-31 01:04:32 -0400

    back from edit x

--- src/components/tuiter/edit-profile/index.js
+++ src/components/tuiter/edit-profile/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 const EditProfile = () => {
     return(
         <>
-            <div>
-                <h5>edit</h5>
+            <div className="card">
+                <div className="card-body row">
+                    <span className="float-start">
+                        <Link to="/tuiter/profile">
+                            <i className="fa-solid fa-xmark"/>
+                        </Link>
+                    </span>
+                </div>
+
             </div>
         </>
     );
