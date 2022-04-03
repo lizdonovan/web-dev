ceae2ba24443a9690382598e5e040094ace8ce50
donovanel <donovan.el@northeastern.edu>
2022-04-02 20:11:33 -0400

    HTTP CONTROLLERS

--- full-stack-developer-server-node/server.js
+++ full-stack-developer-server-node/server.js
@@ -1,5 +1,7 @@
-import express from 'express';
+import express from 'express'
+import helloController
+    from "./controllers/hello-controller.js";
+
 const app = express();
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+helloController(app);
 app.listen(4000);
