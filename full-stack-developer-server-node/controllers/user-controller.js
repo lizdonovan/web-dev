c3480a6cef44b5919d22c7edc11357046bedd3bb
donovanel <donovan.el@northeastern.edu>
2022-04-05 22:40:28 -0400

    rest of section 3

--- full-stack-developer-server-node/controllers/user-controller.js
+++ full-stack-developer-server-node/controllers/user-controller.js
@@ -2,6 +2,7 @@ import people from "./users/users.js";
 let users = people;
 const userController = (app) => {
     app.get('/api/users', findAllUsers);
+    app.get('/api/users/:uid', findUserById);
 }
 
 const findAllUsers = (req, res) => {
@@ -13,8 +14,16 @@ const findAllUsers = (req, res) => {
     res.json(users);
 }
 
-const findUsersByType = () => {
+const findUsersByType = (type) => {
+    return users
+        .filter((user) => type === user.type)
+        .map((name) => name.username);
+}
 
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users.find(u => u._id === userId);
+    res.json(user);
 }
 
 export default userController;
