a1f8e83e81ed079f6d04cd4c32f088dcfadf756d
donovanel <donovan.el@northeastern.edu>
2022-04-18 17:33:22 -0400

    fixed issue, I think branches got confused

--- src/components/tuiter/whotofollow/index.js
+++ src/components/tuiter/whotofollow/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-
+import who from "./who.json"
 import WhoToFollowListItem from "./whotofollowlistitem";
 
 
-const WhoToFollowList = (who) => {
+const WhoToFollowList = () => {
     return (
         <>
             <ul className={`list-group`}>
                 <li className={`list-group-item bg-light`}>Who to follow</li>
 
                 {
-                who.map(who => {
+                    who.map(who => {
                     return (
                         <WhoToFollowListItem who={who}/>
                     );
